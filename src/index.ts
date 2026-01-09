d418a5817271b75ebed17be09133c7accc60e0ae
JunDevl <juninhoplay.pedone@gmail.com>
2026-01-08 23:19:45 -0300

    Started designing the website

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
 import "./style.css";
-
-import { Best2, test1 } from "./tester";
-
-const btn = document.querySelector("#test");
-
-btn.addEventListener("click", () => console.log("ok"));
-
-const ata: any = "boo";
-
-function boo(ava: any) {
-	return true;
-}
--- src/tester.ts
+++
@@ -1,7 +0,0 @@
-export function test1() {
-	return 1;
-}
-
-export function Best2() {
-	return 2;
-}
